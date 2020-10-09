706f12a22da09de8153cd5a40864e4a7ebf8ebc6
kraynova98 <liesehund@yandex.ru>
2020-10-09 18:01:34 +0300

    Добавлена функция reset()

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
       signs: ['+', '-', '/', '*', '.', '='],
       result: ''
     },
     methods: {
       setNumber: function(number) {
-        this.result += number; 
+        this.result += String(number); 
       },
       getResult: function(sign) {
         sign == "=" ? this.result = eval(this.result) : this.setNumber(sign);
+      },
+      reset: function() {
+        this.result = '';
       }
     }
   })

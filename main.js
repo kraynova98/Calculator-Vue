120e06d724e1f1fd3989d61dd178f642d6357241
kraynova98 <liesehund@yandex.ru>
2020-10-09 11:56:38 +0300

    Добавлены стили, разметка и некоторые функции

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Привет, Vue!'
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      result: ''
+    },
+    methods: {
+      setNumber: function(number) {
+        this.result += number; 
+      },
+
     }
   })
